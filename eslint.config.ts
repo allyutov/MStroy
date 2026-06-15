34758b74ad7f4c544e0376cd6ef0917bdb1d7a5c
Alexander Lyutov <lyutov.alexander@e-queo.com>
2026-06-15 12:13:03 +0300

    init project

---
+++ eslint.config.ts
@@ -0,0 +1,54 @@
+import { globalIgnores } from 'eslint/config'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import pluginVue from 'eslint-plugin-vue'
+import pluginVitest from '@vitest/eslint-plugin'
+import pluginOxlint from 'eslint-plugin-oxlint'
+import skipFormatting from 'eslint-config-prettier/flat'
+
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{vue,ts,mts,tsx}'],
+  },
+
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+  ...pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['src/**/*.{test,spec}.{ts,tsx}'],
+  },
+
+  ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
+
+  skipFormatting,
+
+  {
+    name: 'app/custom-rules',
+    rules: {
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+  
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+  
+      'vue/multi-word-component-names': 'off',
+      'vue/no-v-html': 'warn',
+      'vue/component-name-in-template-casing': ['warn', 'PascalCase'],
+  
+      eqeqeq: ['error', 'always'],
+  
+      'prefer-const': 'warn',
+      'no-var': 'error',
+
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+)
